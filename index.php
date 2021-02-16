54393ccddd3a2cea75aed72934742afc86c0f728
romyagon <romyagon.0845@gmail.com>
2021-02-16 15:45:10 +0700

    88

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
-<?php
-echo "Hello LINE BOT";
+<!DOCTYPE html>
+<head>
+<title>ทดสอบ รับค่า UserID</title>
+</head>
+<body>
+    <form action="botpush.php" method="POST" name="">\
+    <td><input type="submit"type="submit"value="ส่งค่า"></td>
+
+    </form>
+     
+</body>
+</html>
